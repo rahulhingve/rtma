8068a373196213b2c80f6eddd1dfed3a4d0eacde
unknown <rahulpawar2001.rp@gmail.com>
2024-07-10 21:46:26 +0530

    migrated to nextjs app from turborepo coz thats hard to manage

---
+++ app/(pages)/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,58 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+
+    ],
+    callbacks: {
+        async signIn({ user, account }: {
+            user: {
+                email: string,
+                name: string
+            },
+            account: {
+                provider: "google" | "github"
+            }
+        }) {
+            if (!user || !user.email) {
+                return false;
+            }
+
+            await prisma.user.upsert({
+                select: {
+                    id: true
+                },
+                where: {
+                    email: user.email
+                },
+                create: {
+                    email: user.email,
+                    name: user.name,
+                    auth_type: account.provider === "google" ? "Google" : "Github"
+                },
+                update: {
+                    name: user.name,
+                    auth_type: account.provider === "google" ? "Google" : "Github"
+                }
+            });
+            return true;
+
+
+        },
+        secret:process.env.NEXTAUTH_SECRET
+    }
+}
--- apps/frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
-  typescript:{
-    ignoreBuildErrors:true
-  }
-};
