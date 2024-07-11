b17b101a1e922aacbcd12322a1dc8334a5b19b6c
unknown <rahulpawar2001.rp@gmail.com>
2024-07-11 11:35:02 +0530

    Added session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import SessionWraper from "./components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+
+    <SessionWraper>
+      <html lang="en">
+        <body className={inter.className}>
+          
+          {children}
+        
+          </body>
+      </html>
+    </SessionWraper>
+
   );
 }
