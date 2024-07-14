e13af2ac7059762624fac8dd51494f7c794bdcb7
unknown <rahulpawar2001.rp@gmail.com>
2024-07-14 17:55:22 +0530

    added new component meetingsetup

---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { VideoPreview, useCall } from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
+
+const MeetingSetup = () => {
+    const [isMicCamToggledOn, setisMicCamToggledOn] = useState(false)
+    const call = useCall();
+    // if(!call){
+    //     throw new Error('usecall must be used wiithin StreamCall component')
+    // }
+    useEffect(() => {
+        if(isMicCamToggledOn){
+            call?.camera.disable();
+            call?.microphone.disable();
+
+        }else{
+            call?.camera.enable();
+            call?.microphone.enable();
+        }
+
+    }, [isMicCamToggledOn, call?.camera, call?.microphone])
+    return (
+        <div className='flex h-full w-full flex-col items-center justify-center gap-3 text-white '>
+
+            <h1 className='text-2xl font-bold'>Setup</h1>
+            <VideoPreview />
+        </div>
+    )
+}
+
+export default MeetingSetup
